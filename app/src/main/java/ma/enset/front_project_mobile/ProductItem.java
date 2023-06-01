fb8d4d6ad10127b2ff3d06ca53f12385d59d26f1
AKASMIOU Ouassima <o.akasmiou@etu.enset-media.ac.ma>
2023-06-02 00:45:14 +0100

    adding peoductDetailac

--- app/src/main/java/ma/enset/front_project_mobile/ProductItem.java
+++ app/src/main/java/ma/enset/front_project_mobile/ProductItem.java
@@ -4,6 +4,7 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class ProductItem implements Parcelable {
+    private Long id;
     private String name;
     private String description;
     private double price;
