f2209818213e0c80425c5a24e999f12c577e03ca
DOUHIChaimae <c.douhi@etu.enset-media.ac.ma>
2023-06-02 01:56:51 +0100

    backButton

--- app/src/main/java/ma/enset/front_project_mobile/ProductDetailsActivity.java
+++ app/src/main/java/ma/enset/front_project_mobile/ProductDetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,8 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_details);
 
         Button addToCartButton=findViewById(R.id.button_add_to_cart);;
+        ImageButton backButton = findViewById(R.id.backButton);
+
         addToCartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +25,13 @@ public class ProductDetailsActivity extends AppCompatActivity {
                 v.getContext().startActivity(intent);
             }
         });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProductDetailsActivity.this, ProductListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
